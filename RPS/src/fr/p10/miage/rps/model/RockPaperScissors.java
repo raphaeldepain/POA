9e8d8e74d846c273e254d53843b321e1025f7fae
raphaeldepain <rdepain@gmail.com>
2018-10-22 17:20:55 +0200

    Ajout des DataProviders dans les tests

--- RPS/src/fr/p10/miage/rps/model/RockPaperScissors.java
+++ RPS/src/fr/p10/miage/rps/model/RockPaperScissors.java
@@ -1,8 +1,19 @@
 package fr.p10.miage.rps.model;
 
+import static fr.p10.miage.rps.model.RPSEnum.PAPER;
+import static fr.p10.miage.rps.model.RPSEnum.ROCK;
+import static fr.p10.miage.rps.model.RPSEnum.SCISSORS;
+
 public class RockPaperScissors {
     Result play(RPSEnum p1, RPSEnum p2){
-        return Result.LOST;
+        if(p1==p2)
+        return Result.TIE;
+        if((p1==SCISSORS && p2==PAPER) || (p1==PAPER && p2==ROCK) || (p1==ROCK && p2==SCISSORS)){
+            return Result.WIN;
+        }
+        else{
+            return Result.LOST;
+        }
     }
 
 }
--- RPS/test/fr/p10/miage/rps/model/RockPaperScissorsTest.java
+++ RPS/test/fr/p10/miage/rps/model/RockPaperScissorsTest.java
@@ -1,19 +1,46 @@
 package fr.p10.miage.rps.model;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import static fr.p10.miage.rps.model.RPSEnum.PAPER;
 import static fr.p10.miage.rps.model.RPSEnum.ROCK;
+import static fr.p10.miage.rps.model.RPSEnum.SCISSORS;
 import static fr.p10.miage.rps.model.Result.LOST;
+import static fr.p10.miage.rps.model.Result.TIE;
 import static fr.p10.miage.rps.model.Result.WIN;
 import static org.testng.Assert.*;
 
 public class RockPaperScissorsTest {
 
     RockPaperScissors rps;
+    @DataProvider(name = "winData")
+    public Object[][] createWinData() {
+        return new Object[][] {
+                { PAPER, ROCK},
+                { ROCK, SCISSORS},
+                { SCISSORS, PAPER},
+        };
+    }
+
+    @DataProvider(name = "lostData")
+    public Object[][] createLostData() {
+        return new Object[][] {
+                { PAPER, SCISSORS},
+                { ROCK, PAPER},
+                { SCISSORS, ROCK},
+
+        };
+    }
+
+    @DataProvider(name = "tieData")
+    public Object[][] createTieData() {
+        return new Object[][] {
+                { PAPER, PAPER},
+                { ROCK, ROCK},
+                { SCISSORS, SCISSORS},
+
+        };
+    }
 
     @BeforeMethod
     public void setUp() {
@@ -26,9 +53,21 @@ public class RockPaperScissorsTest {
 
     }
 
-    @Parameters({"paper","rock"})
-    @Test
-    public void testWinPlay(String p1, String p2) {
-        assertEquals(rps.play(RPSEnum.valueOf(p1),RPSEnum.valueOf(p2)), WIN);
+
+    @Test(dataProvider = "winData")
+    public void testWinPlay( RPSEnum p1 , RPSEnum p2) {
+        assertEquals(rps.play(p1,p2), WIN);
     }
+
+    @Test(dataProvider = "lostData")
+    public void testLostPlay( RPSEnum p1 , RPSEnum p2) {
+        assertEquals(rps.play(p1,p2), LOST);
+    }
+
+    @Test(dataProvider = "tieData")
+    public void testTiePlay( RPSEnum p1 , RPSEnum p2) {
+        assertEquals(rps.play(p1,p2), TIE);
+    }
+
+
 }
